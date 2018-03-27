4af7f88ef097d8f08eb0826ccc77f1b91e42e571
philpatt <phillip.s.patterson1@gmail.com>
2018-03-26 18:42:08 -0700

    added nav styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import '../index.css';
 import Popular from './Popular.js';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+
 
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        <Popular />
-      </div>
+      <Router>
+        <div className="container">
+          <Route path='/popular' component = {Popular} />
+        </div>
+      </Router>
     );
   }
 }
