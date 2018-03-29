24beb4249eef0621bcda6b1863923a830e9c5ff7
philpatt <phillip.s.patterson1@gmail.com>
2018-03-29 16:37:17 -0700

    deploying to heorku

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,21 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-    entry: './app/index.js',
-}
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index_bundle.js'
+    },
+    module: {
+        rules: [
+            { test: /\.(js)$/, use: 'babel-loader' },
+            { test: /\.css$/, use: ['style-loader', 'css-loader'] }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        })
+    ]
+};
