c361a80bb0536b1952fdfa1faccdcce2a0675746
philpatt <phillip.s.patterson1@gmail.com>
2018-03-29 20:51:19 -0700

    added package.json

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+
 import '../index.css';
 import Nav from './Nav';
-import Home from './Home';
 import Battle from './Battle';
 import Results from './Results';
 import Popular from './Popular';
+import Home from './Home';
 
 
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+
 
 
 
