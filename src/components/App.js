49aa610f76aac3f8b834ca8df2ae370a8debd6c1
philpatt <phillip.s.patterson1@gmail.com>
2018-04-02 17:24:08 -0700

    moved nav import above home

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
 import '../index.css';
+import Nav from './Nav';
+
 import Home from './Home';
 
-import Nav from './Nav';
 import Battle from './Battle';
 import Results from './Results';
 import Popular from './Popular';
