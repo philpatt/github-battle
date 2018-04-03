a58c18339e064a974d9d419401400fbbae239192
philpatt <phillip.s.patterson1@gmail.com>
2018-04-02 17:16:37 -0700

    added home

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
 import '../index.css';
+import Home from './Home';
+
 import Nav from './Nav';
 import Battle from './Battle';
 import Results from './Results';
 import Popular from './Popular';
-// import Home from './Home';
+
 
 
 
@@ -20,7 +22,6 @@ class App extends Component {
         <div className="container">
           <Nav />
           <Switch>
-            {/* <Route exact path='/' component={} /> */}
             
             <Route exact path='/' component={Battle} />
             <Route exact path='/battle/results' component={Results} />         
